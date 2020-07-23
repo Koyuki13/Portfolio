a7866c42462dabdd5bfe8def052afe149ee30f36
Natacha <villeneuve.natacha@gmail.com>
2020-07-23 10:22:21 +0200

    style de la page d'accueil

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,5 +14,3 @@ require('@fortawesome/fontawesome-free/js/all.js');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 require('bootstrap');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,8 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addStyleEntry('home', './assets/scss/home.scss')
+    .addStyleEntry('login', './assets/scss/login.scss')
+    // .addEntry('images/portfolio_background', './assets/images/portfolio_background.jpg')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
